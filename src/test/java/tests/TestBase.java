100a3299097987e64b775e20f845bb1f97a2fad1
Taron Gukasyan <funkadelica@mail.ru>
2025-12-11 15:56:58 +0300

    corrections

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,8 +13,7 @@ public class TestBase {
 
     @BeforeAll
     public static void setUp() {
-        Configuration.headless = true;
-        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         RestAssured.baseURI = "https://demoqa.com";
         Configuration.baseUrl="https://demoqa.com";
 
